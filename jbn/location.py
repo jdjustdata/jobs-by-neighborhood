36a8e1d1ed91542df7e372d0e4b6e1602f613bef
Thomp <thomp@protonmail.com>
2018-03-05 16:18:54 -0600

    update Location object for Mapbox api

--- jbn/location.py
+++ jbn/location.py
@@ -1,36 +1,39 @@
 import os
+import api_keys
+import requests
+import json
 
-api_key = None
-with open(os.getcwd()+"/keys.txt", "r") as key_doc:
-    api_key = key_doc.read()
+
+api_key = api_keys.mapbox_key
+
 
 class Location(object):
 
     def __init__(self, neighborhood):
         self.url = None
-        self.head =  "https://maps.googleapis.com/maps/api/staticmap"
+        self.head = "https://api.mapbox.com/geocoding/v5/"
+        self.map_type = "mapbox.places/"
+        self.response_type = ".json"
         self.zoom = str(15)
-        self.size = "500x400"
         self.neighborhood = neighborhood
-        self.city = "Chicago"
-        self.state = "IL"
-
+        # leading spaces to separate parameters
+        self.city = " Chicago"
+        self.state = " IL"
+        self.coordinates = None
 
     def build_url(self):
-        self.url = self.head + "?" + "center=" + self.neighborhood + ","+ self.city + "," + self.state + "&zoom=" + self.zoom + "&size=" + self.size + "&key=" + api_key
+        self.url = self.head + self.map_type + self.neighborhood + self.city + \
+                   self.state + self.response_type + "?access_token=" + api_key
 
         self.url.replace(" ", "+").replace(",", "%2C")
         return self
 
-
-class Embedded(Location):
-    def __init__(self, neighborhood):
-        super(Embedded, self).__init__(neighborhood)
-        self.head = "https://www.google.com/maps/embed/v1/"
-        self.mode = "search"
-
-    def build_url(self):
-        self.url = self.head + self.mode + "?" + "q=" + self.neighborhood + ","+ self.city + "," + self.state + "&key=" + api_key
-
-        self.url.replace(" ", "+").replace(",", "%2C")
+    def retrieve_json(self):
+        r = requests.get(self.url)
+        j = r.json()
+        # reverse coordinates order
+        latitude = j['features'][0]['center'][1]
+        longitude = j['features'][0]['center'][0]
+        self.coordinates = [latitude, longitude]
         return self
+
