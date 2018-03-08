d34ed5cab3505a46f9e5a1b2eab5aab56198d218
Thomp <thomp@protonmail.com>
2018-03-08 07:49:50 -0600

    shorter db name

--- db_script.py
+++ db_script.py
@@ -5,11 +5,11 @@ from random import randint
 from random import choice
 import string
 
-db_name = "jobs_by_neighborhood.db"
+db_name = "jobs.db"
 db_path = getcwd() + "/" + db_name
 
 jobs_header = '''CREATE TABLE 'Jobs'
-(Title TEXT, Company TEXT, Address TEXT, Neighborhood TEXT, Shift TEXT, Description INT, PostedDate DATE)'''
+(PostedDate DATE, Title TEXT, Company TEXT, Address TEXT, Neighborhood TEXT, Shift TEXT, Description INT)'''
 
 def create_table(query):
     conn = sqlite3.connect(db_name)
@@ -20,7 +20,7 @@ def create_table(query):
 
 
 def add_job(location):
-    conn = sqlite3.connect('jobs_by_neighborhood.db')
+    conn = sqlite3.connect(db_name)
     c = conn.cursor()
     rand_number = randint(0, 1000)
     title = "Job no." + str(rand_number)
@@ -38,7 +38,7 @@ def add_job(location):
     description = ''.join(choice(
     string.ascii_letters + string.digits) for n in range(16))
     posted_date = "2018-03-06"
-    job = [title, company, address, neighborhood, shift, description, posted_date]
+    job = [posted_date, title, company, address, neighborhood, shift, description]
     c.execute("INSERT INTO 'Jobs' VALUES (?,?,?,?,?,?,?)", job)
     conn.commit()
     conn.close()
