06ebcf056f7397ba7ab02d6ab25ab18925ebf3df
Joshua Daniel <joshuadaniel@fuller.edu>
2018-03-13 23:00:08 -0500

    removed venv from remote directory

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jvod4\desktop\chihacknight\jobs-by-neighborhood\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
