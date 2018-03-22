e93f454a4fc025504c943759a618bdd04296a0a2
Thomp <thomp@protonmail.com>
2018-03-21 20:40:40 -0500

    the pins code is a mess, but it works. will clean up soon.

--- apps/home/views.py
+++ apps/home/views.py
@@ -15,6 +15,46 @@ else:
 
 from ..geography.views import Location
 
+# MT Code to be moved elsewhere
+from ..jobs.views import Job
+from os import getcwd
+import sqlite3
+db_path = getcwd() + "/jobs.db"
+
+def retrieve_jobs(db):
+    query = """SELECT * FROM Jobs"""
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    print(db_path)
+    retrieved = c.execute(query).fetchall()
+    for r in retrieved:
+        print(r)
+    conn.commit()
+    conn.close()
+    return retrieved
+
+def convert_db_row_to_dict(db_data):
+    dictionary = {
+        'title': db_data[1],
+        'postedDate': db_data[0],
+        'company': db_data[2],
+        'location': db_data[3],
+        # Neighborhood (db_data[4]) is not used
+        'shift': db_data[5],
+        'description': db_data[6],
+        'tags': ""
+    }
+    return dictionary
+
+
+def build_jobs(db_rows):
+    jobs_list = []
+    for row in db_rows:
+        jobs_list.append(Job(convert_db_row_to_dict(row)))
+    return jobs_list
+
+
+
 def index(request):
     title = DOMAIN_NAME
     neighborhood = request.GET.get('neighborhood', 'Wicker Park').replace('+', ' ')
@@ -23,11 +63,18 @@ def index(request):
     location = Location(neighborhood).get_coordinates()
 
     # add_job(location)
+    # Mike T
+    db_data = retrieve_jobs(db_path)
+    jobs = build_jobs(db_data)
+    for j in jobs:
+        print(j.location.coordinates)
 
     context = {
         'title': title,
         'location': location,
-        'api_key': MAPBOX_KEY
+        'api_key': MAPBOX_KEY,
+        # MT Code
+        'jobs': jobs
     }
     return render(request, "home/index.html", context)
 
--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -4,3 +4,21 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from ..geography.views import Location
+
+class Job(object):
+
+    def __init__(self, job_data):
+        self.title = job_data.get('title')
+        self.posted_date = job_data.get('postedDate')
+        self.company = job_data.get('company')
+        # this will be a Location() object
+        self.location = Location(job_data.get('location')).get_coordinates()
+        self.shift = job_data.get('shift')
+        self.description = job_data.get('description')
+        self.tags = job_data.get('tags')
+
+
+    def add_to_database(self):
+        # TODO: move add_job() from db_script.py here
+        pass
