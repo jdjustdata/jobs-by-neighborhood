d04b7336cce4d4d05aa8cd608a1baed1a9a8d370
Joshua Daniel <joshuadaniel@fuller.edu>
2018-04-07 16:09:38 -0500

    add businesses, add locations with api call to store geocode data, update locations, view all locations by neighborhood, view map of location

--- apps/app_form_templates.py
+++ apps/app_form_templates.py
@@ -1,67 +1,75 @@
 from django import forms
 from django.core.validators import RegexValidator
 
+import re
+
 
 class USPhoneNumberMultiWidget(forms.MultiWidget):
     """
     A Widget that splits US Phone number input into three <input type='text'> boxes.
     """
 
-    def __init__(self, attrs=None):
+    def __init__(self, attrs={}):
         widgets = (
             forms.TextInput(
                 attrs={
                     'size': '3',
                     'maxlength': '3',
-                    'class': 'phone',
+                    'class': 'phone text-center',
                     'required': False,
                 }),
             forms.TextInput(
                 attrs={
                     'size': '3',
                     'maxlength': '3',
-                    'class': 'phone',
+                    'class': 'phone text-center',
                     'required': False,
                 }),
             forms.TextInput(
                 attrs={
                     'size': '4',
                     'maxlength': '4',
-                    'class': 'phone',
+                    'class': 'phone text-center',
                     'required': False,
                 }),
             forms.TextInput(
                 attrs={
-                    'size': '5',
-                    'maxlength': '5',
-                    'class': 'phone',
+                    'size': '8',
+                    'maxlength': '8',
+                    'class': 'phone text-left',
                     'required': False,
                 }),
         )
-        super(USPhoneNumberMultiWidget, self).__init__(widgets, attrs)
+        super(USPhoneNumberMultiWidget, self).__init__(widgets)
 
 
     def decompress(self, value):
         if value:
-            return value.split('-')
-        return (None, None, None)
+            return re.split('-| ext.', value)
+        return (None, None, None, None)
 
     def value_from_datadict(self, data, files, name):
-        value = [u'', u'', u'']
+        value = [u'', u'', u'', u'']
         # look for keys like name_1, get the index from the end
         # and make a new list for the string replacement values
         for d in filter(lambda x: x.startswith(name), data):
             index = int(d[len(name) + 1:])
             value[index] = data[d]
-        if value[0] == value[1] == value[2] == u'':
+        if value[0] == value[1] == value[2] == value[3] == u'':
             return None
-        return u'%s-%s-%s' % tuple(value)
+        if value[3] == u'':
+            # print u'{}-{}-{}'.format(value[0], value[1], value[2])
+            return u'{}-{}-{}'.format(value[0], value[1], value[2])
+        else:
+            # print u'{}-{}-{} ext.{}'.format(value[0], value[1], value[2], value[3])
+            return u'{}-{}-{} ext.{}'.format(value[0], value[1], value[2],
+                                         value[3])
 
 
 class PhoneField(forms.MultiValueField):
     widget = USPhoneNumberMultiWidget
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         # Define one message for all fields.
         error_messages = {
             'incomplete': 'Enter an area code and a phone number.',
@@ -69,32 +77,32 @@ class PhoneField(forms.MultiValueField):
         # Or define a different message for each field.
         fields = (
             forms.CharField(
+                required=False,
                 error_messages={'incomplete': 'Enter an area code.'},
                 validators=[
                     RegexValidator(r'^[0-9]{3}$', 'Enter a valid area code.'),
                 ],
-                required=False,
             ),
             forms.CharField(
+                required=False,
                 error_messages={'incomplete': 'Enter a phone number.'},
                 validators=[
                     RegexValidator(r'^[0-9]{3}$',
                                    'Enter a valid phone number.')
                 ],
-                required=False,
             ),
             forms.CharField(
+                required=False,
                 validators=[
                     RegexValidator(r'^[0-9]{4}$',
                                    'Enter a valid phone number.')
                 ],
-                required=False,
             ),
             forms.CharField(
-                validators=[
-                    RegexValidator(r'^[0-9]+$', 'Enter a valid extension.')
-                ],
                 required=False,
+                validators=[
+                    RegexValidator(r'^[0-9]{0,8}$', 'Enter a valid extension.')
+                ],
                 label="Extension"
             ),
         )
@@ -102,8 +110,10 @@ class PhoneField(forms.MultiValueField):
             error_messages=error_messages,
             fields=fields,
             required=False,
-            require_all_fields=False)
+            require_all_fields=False,
+            *args, **kwargs)
 
-
-        def compress(self, data_list):
+    def compress(self, data_list):
+        if data_list:
             return '-'.join(data_list)
+        return None
--- apps/business/forms.py
+++ apps/business/forms.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from datetime import date, datetime
 
-from models import Business
+from models import Business, Location
 from ..app_form_templates import USPhoneNumberMultiWidget, PhoneField
 
 
@@ -12,9 +12,8 @@ def today_as_string():
     return today
 
 class NewBusiness(ModelForm):
-    def __init__(self):
-        super(NewBusiness, self).__init__()
-        self.fields['phone'] = PhoneField()
+    def __init__(self, data=None, *args, **kwargs):
+        super(NewBusiness, self).__init__(data, *args, **kwargs)
 
         self.fields['name'].help_text = "Name of your business"
         self.fields['industry'].help_text = "What is the primary industry for your business?"
@@ -26,10 +25,6 @@ class NewBusiness(ModelForm):
             'name',
             'industry',
             'subindustry',
-            'street',
-            'city',
-            'state',
-            'zipcode',
             'website',
             'email',
             'phone'
@@ -40,3 +35,27 @@ class NewBusiness(ModelForm):
             'phone': USPhoneNumberMultiWidget()
         }
 
+
+class NewLocation(ModelForm):
+    def __init__(self, data=None, *args, **kwargs):
+        super(NewLocation, self).__init__(data, *args, **kwargs)
+
+        self.fields['name'].help_text = "If your business has multiple locations, please name this location."
+        self.fields['is_primary'].label = "Primary Location"
+        self.fields['is_primary'].help_text = "Is this the primary (default) location for your business?"
+        self.fields['phone'].help_text = "Does this location have a unique phone number?"
+
+    class Meta:
+        model = Location
+        fields = [
+            'name',
+            'is_primary',
+            'street',
+            'city',
+            'state',
+            'zipcode',
+            'phone'
+        ]
+        widgets = {
+            'phone': USPhoneNumberMultiWidget()
+        }
