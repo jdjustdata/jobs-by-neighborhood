3489d3688f0aa038c632b35b345b099ab1278249
Joshua Daniel <joshuadaniel@fuller.edu>
2018-04-14 22:02:38 -0500

    updated procfile for deployment

--- main/settings.py
+++ main/settings.py
@@ -120,6 +120,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     # os.path.join("/apps/main/", "static"),
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'content_files'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
