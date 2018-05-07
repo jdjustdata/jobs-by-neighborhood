a48227a64bda8aaf6f00a36eb8233313bd90a370
Thomp <thomp@protonmail.com>
2018-05-07 07:52:38 -0500

    uncomment custom Job() and Listings() classes, as they are used by the map. Also, running into db error b/c no table named Jobs

--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -14,7 +14,7 @@ else:
 
 from ..geography.views import Location
 from ..business.models import BusinessManager
-from models import Job
+import models
 
 from os import getcwd
 import sqlite3
@@ -47,7 +47,7 @@ def root(request):
         #INSERT NEW JOB INTO DATABASE
         print('printing request.POST')
         #job = [posted_date, title, company, address, neighborhood, shift, description]
-        job = Job(
+        job = models.Job(
             title = request.POST.get('title', ''),
             business = request.POST.get('company', ''),
             # TODO: Add to web form
@@ -109,55 +109,55 @@ def insert_into_db(job):
     conn.commit()
     conn.close()
 
-# class Job(object):
-#
-#     def __init__(self, job_data):
-#         self.title = job_data.get('title')
-#         self.posted_date = job_data.get('postedDate')
-#         self.company = job_data.get('company')
-#         # this will be a Location() object
-#         self.location = Location(job_data.get('location')).get_coordinates()
-#         self.shift = job_data.get('shift')
-#         self.description = job_data.get('description')
-#         self.tags = job_data.get('tags')
-#
-#
-#     def add_to_database(self):
-#         # TODO: move add_job() from db_script.py here
-#         pass
-#
-#
-#
-# class Listings(list):
-#
-#     def __init__(self, default_list=[]):
-#         super(Listings, self).__init__(default_list)
-#         self.db = getcwd() + "/jobs.db"
-#         self.raw_data = None
-#
-#     def retrieve_jobs(self):
-#         query = """SELECT * FROM Jobs"""
-#         conn = sqlite3.connect(self.db)
-#         c = conn.cursor()
-#         self.raw_data = retrieved = c.execute(query).fetchall()
-#         conn.commit()
-#         conn.close()
-#         return self
-#
-#     def convert_db_row_to_dict(self, db_data):
-#         dictionary = {
-#             'title': db_data[2],
-#             'postedDate': db_data[1],
-#             'company': db_data[3],
-#             'location': db_data[4],
-#             # Neighborhood (db_data[5]) is not used
-#             'shift': db_data[6],
-#             'description': db_data[7],
-#             'tags': ""
-#         }
-#         return dictionary
-#
-#     def build_jobs(self):
-#         for row in self.raw_data:
-#             self.append(Job(self.convert_db_row_to_dict(row)))
-#         return self
+class Job(object):
+
+    def __init__(self, job_data):
+        self.title = job_data.get('title')
+        self.posted_date = job_data.get('postedDate')
+        self.company = job_data.get('company')
+        # this will be a Location() object
+        self.location = Location(job_data.get('location')).get_coordinates()
+        self.shift = job_data.get('shift')
+        self.description = job_data.get('description')
+        self.tags = job_data.get('tags')
+
+
+    def add_to_database(self):
+        # TODO: move add_job() from db_script.py here
+        pass
+
+
+
+class Listings(list):
+
+    def __init__(self, default_list=[]):
+        super(Listings, self).__init__(default_list)
+        self.db = getcwd() + "/jobs.db"
+        self.raw_data = None
+
+    def retrieve_jobs(self):
+        query = """SELECT * FROM Jobs"""
+        conn = sqlite3.connect(self.db)
+        c = conn.cursor()
+        self.raw_data = retrieved = c.execute(query).fetchall()
+        conn.commit()
+        conn.close()
+        return self
+
+    def convert_db_row_to_dict(self, db_data):
+        dictionary = {
+            'title': db_data[2],
+            'postedDate': db_data[1],
+            'company': db_data[3],
+            'location': db_data[4],
+            # Neighborhood (db_data[5]) is not used
+            'shift': db_data[6],
+            'description': db_data[7],
+            'tags': ""
+        }
+        return dictionary
+
+    def build_jobs(self):
+        for row in self.raw_data:
+            self.append(Job(self.convert_db_row_to_dict(row)))
+        return self
