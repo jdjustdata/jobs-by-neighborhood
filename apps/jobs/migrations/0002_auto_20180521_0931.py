0ae5c0a0e5d1f48f73f113f652b6cd199b1e6e70
Thomp <thomp@protonmail.com>
2018-07-14 14:28:21 -0500

    change back

--- apps/jobs/migrations/0002_auto_20180521_0931.py
+++ apps/jobs/migrations/0002_auto_20180521_0931.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterField(
             model_name='job',
-            name='location_id',
+            name='location',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='JobLocation', to='business.Location'),
         ),
     ]
